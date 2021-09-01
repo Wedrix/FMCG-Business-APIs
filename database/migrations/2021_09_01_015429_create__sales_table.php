6776fe80593c9952e9d0c04761c88e0efe00a4cc
kennedy owurani <joerenin2016@outlook.com>
2021-09-01 03:04:11 +0000

    Created user, reciept, sales and audit tables

---
+++ database/migrations/2021_09_01_015429_create__sales_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_sales', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('quantity');
+            $table->timestamp('s_date');
+            $table->foreign('shop_id')
+                ->references('id')
+                ->on('shops')
+                ->onDelete('cascade');
+            $table->foriegn('user_id')
+                ->references('id')
+                ->on('user')
+                ->onDelete('cascade');
+            $table->foriegn('reciept_id')
+                ->references('id')
+                ->on('reciept')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_sales');
+    }
+}
--- database/migrations/2021_09_01_021744_create_reciept_table.php
+++ database/migrations/2021_09_01_021744_create_reciept_table.php
@@ -3,8 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use app\Enum\Role;
-class CreateUsersTable extends Migration
+
+class CreateRecieptTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reciept', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
             $table->string('name');
             $table->string('phone_number');
-            $table->tinyInteger('role')->unsigned()->default(Role::Member);
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -34,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reciept');
     }
 }
